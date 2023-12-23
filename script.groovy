3bd82b5f116a4fca10f696607eef427140be0c60
Oumayma Charrad <130283344+OumaymaCharrad1999@users.noreply.github.com>
2023-12-23 17:38:16 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -46,9 +46,9 @@ def pushToDeploymentGitHub() {
     echo "Pushing to Deployment GitHub..."
     sh "git clone git@github.com:Cloudees/deployment.git"
     sh "CURRENT_VERSION=\$(grep 'image: oumaymacharrad/data-seed-job' deployment/microservices/playlist/deployment-playlist.yaml | awk -F: '{print \$3}' | cut -d'@' -f1)"
-    sh "sed -i \"s| image: oumaymacharrad/data-seed-job:${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:${IMAGE_VERSION}|\" deployment/microservices/playlist/deployment-playlist.yaml"
+    sh "sed -i \"s| image: oumaymacharrad/data-seed-job:\${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:\${IMAGE_VERSION}|\" deployment/microservices/playlist/deployment-playlist.yaml"
     sh "CURRENT_VERSION=\$(grep 'image: oumaymacharrad/data-seed-job' deployment/microservices/videos/deployment-videos.yaml | awk -F: '{print \$3}' | cut -d'@' -f1)"
-    sh "sed -i \"s| image: oumaymacharrad/data-seed-job:${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:${IMAGE_VERSION}|\" deployment/microservices/videos/deployment-videos.yaml"
+    sh "sed -i \"s| image: oumaymacharrad/data-seed-job:\${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:\${IMAGE_VERSION}|\" deployment/microservices/videos/deployment-videos.yaml"
     sh """
     cd deployment
     git commit -am 'Increment Version to ${IMAGE_VERSION}'
