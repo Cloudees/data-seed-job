98be86a185fd84a9c50b3fecedd1af2c8c4e3846
Oumayma Charrad <130283344+OumaymaCharrad1999@users.noreply.github.com>
2023-12-23 18:16:20 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -47,7 +47,9 @@ def pushToDeploymentGitHub() {
     sh "git clone git@github.com:Cloudees/deployment.git"
     def currentVersion = sh(script: "grep 'image: oumaymacharrad/data-seed-job' deployment/microservices/playlist/deployment-playlist.yaml | awk -F: '{print \$3}' | cut -d'@' -f1", returnStdout: true)
     env.CURRENT_VERSION = currentVersion
-    sh "sed -i \"s| image: oumaymacharrad/data-seed-job:\${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:\${IMAGE_VERSION}|\" deployment/microservices/playlist/deployment-playlist.yaml"
+    sh """
+    sed -i "s# image: oumaymacharrad/data-seed-job:\${CURRENT_VERSION}# image: oumaymacharrad/data-seed-job:\${IMAGE_VERSION}#" deployment/microservices/playlist/deployment-playlist.yaml
+    """
     sh "sed -i \"s| image: oumaymacharrad/data-seed-job:\${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:\${IMAGE_VERSION}|\" deployment/microservices/videos/deployment-videos.yaml"
     sh """
     cd deployment
