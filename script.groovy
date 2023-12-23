f4adc3a8c7229bcef1c1645a0cfad6656504bee7
Oumayma Charrad <130283344+OumaymaCharrad1999@users.noreply.github.com>
2023-12-23 17:56:00 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -45,7 +45,7 @@ def trivyScan(){
 def pushToDeploymentGitHub() {
     echo "Pushing to Deployment GitHub..."
     sh "git clone git@github.com:Cloudees/deployment.git"
-    sh "currentVersion=\$(grep 'image: oumaymacharrad/data-seed-job' deployment/microservices/playlist/deployment-playlist.yaml | awk -F: '{print \$3}' | cut -d'@' -f1)"
+    def currentVersion = sh(script: "grep 'image: oumaymacharrad/data-seed-job' deployment/microservices/playlist/deployment-playlist.yaml | awk -F: '{print \$3}' | cut -d'@' -f1)
     env.CURRENT_VERSION = currentVersion
     sh "sed -i \"s| image: oumaymacharrad/data-seed-job:\${CURRENT_VERSION}| image: oumaymacharrad/data-seed-job:\${IMAGE_VERSION}|\" deployment/microservices/playlist/deployment-playlist.yaml"
     // sh "currentVersion=\$(grep 'image: oumaymacharrad/data-seed-job' deployment/microservices/videos/deployment-videos.yaml | awk -F: '{print \$3}' | cut -d'@' -f1)"
