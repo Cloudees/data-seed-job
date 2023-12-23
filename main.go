62fa95e4eeabf7e739128f938a9f01dacf65b325
OumaymaCharrad1999 <charradoumayma1@gmail.com>
2023-12-23 11:26:48 +0000

    Increment Version to 1.0.9

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"os"
 )
 
-const version = "1.0.8"
+const version = "1.0.9"
 var microservice = os.Getenv("MICROSERVICE")
 var redisHost = os.Getenv("REDIS_HOST")
 var redisPort = os.Getenv("REDIS_PORT")
